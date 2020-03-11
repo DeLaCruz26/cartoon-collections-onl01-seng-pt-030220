ee34e6762111470bffe0a8bd3e548bc3873078e4
Dametrik Fick <dametrik.fick@gmail.com>
2020-03-11 06:10:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 def summon_captain_planet(veggies)
   veggies.map { |veg| veg == veggies.length }
-  return veggies.upcase
+  return veggies.upcase("!")
 end
 
 def long_planeteer_calls# code an argument here
