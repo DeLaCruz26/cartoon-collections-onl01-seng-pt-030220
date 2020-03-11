5b9c9e8b9c577a4953a40f13e6a05b8ba1bebfc5
Dametrik Fick <dametrik.fick@gmail.com>
2020-03-11 08:18:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(veggies)
-    veggies.map(&:upcase).map {|veg| veg + "!"}
+    veggies.map(&:upcase).map {|veg| veg.upcase + "!"}
     veggies
 end
 
